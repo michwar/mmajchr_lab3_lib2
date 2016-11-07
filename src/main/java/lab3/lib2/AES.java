7ae350c30770dda9caf71f4d6bb12564aa534b04
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-07 22:19:50 +0100

    akreslenie algorytmu

--- src/main/java/lab3/lib2/AES.java
+++ src/main/java/lab3/lib2/AES.java
@@ -4,6 +4,8 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class AES {
+	
+	public static final String ALGORITHM = "AES/CBC/PKCS5Padding";
 
 	void encrypt(InputStream src, OutputStream out) {
 		

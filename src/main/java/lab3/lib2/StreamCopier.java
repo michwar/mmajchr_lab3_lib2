2dd34ec24f7cca633cbe38def343525a01acf189
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-07 22:14:28 +0100

    pola kalsy StreamCopier

--- src/main/java/lab3/lib2/StreamCopier.java
+++ src/main/java/lab3/lib2/StreamCopier.java
@@ -1,5 +1,13 @@
 package lab3.lib2;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class StreamCopier {
+
+	private byte[] buffer;
+	private InputStream in;
+	private OutputStream out;
+	private int total;
 
 }

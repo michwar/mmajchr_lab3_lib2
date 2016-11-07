836f035ab33fa307ac29c2d10d0aa925d1e44d3e
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-07 22:25:50 +0100

    implementacja metody AES.decrypt

--- src/main/java/lab3/lib2/AES.java
+++ src/main/java/lab3/lib2/AES.java
@@ -4,17 +4,18 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
 import javax.crypto.CipherOutputStream;
 
 public class AES {
 	
 	public static final String ALGORITHM = "AES/CBC/PKCS5Padding";
 
-	public int encrypt(InputStream src, OutputStream out) {
+	public int encrypt(InputStream in, OutputStream out) {
 		try {
 			Cipher cipher = Cipher.getInstance(ALGORITHM);
 			try(CipherOutputStream cout = new CipherOutputStream(out, cipher)) {
-				StreamCopier copier = new StreamCopier(src, cout);
+				StreamCopier copier = new StreamCopier(in, cout);
 				return copier.copyAll();
 			}
 		} catch (Exception e) {
@@ -23,8 +24,17 @@ public class AES {
 		}
 	}
 
-	void decrypt(InputStream src, OutputStream out) {
-		
+	public int decrypt(InputStream in, OutputStream out) {
+		try {
+			Cipher cipher = Cipher.getInstance(ALGORITHM);
+			try(CipherInputStream cin = new CipherInputStream(in, cipher)) {
+				StreamCopier copier = new StreamCopier(cin, out);
+				return copier.copyAll();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("No AES support", e);
+		}
 	}
 
 }

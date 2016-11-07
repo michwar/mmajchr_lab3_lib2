a7776cb81a6e87b12eadc0911d947fd264877286
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-07 22:53:05 +0100

    test: po zaszyfrowaniu i odszyfrowaniu dane powinny byc w pierwotnej formie

--- src/test/java/lab3/lib2/AESTest.java
+++ src/test/java/lab3/lib2/AESTest.java
@@ -1,12 +1,20 @@
 package lab3.lib2;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class AESTest {
 	
 	@Test
 	public void test() {
-		;
+		byte[] key = "BardzoTajneHaslo".getBytes();
+		AES aes = new AES();
+		aes.setKey(key);
+		String dane = "BardzoTajneDane1BardzoTajneDane2BardzoTajneDane3";
+		byte[] szyfr = aes.encrypt(dane.getBytes());
+		String dane2 = new String(aes.decrypt(szyfr));
+		assertTrue(dane.equals(dane2));
 	}
 
 }

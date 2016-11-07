fecd5977d8a7c7a63750a6c77266912bf06f036b
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-07 22:24:06 +0100

    implementacja metody AES.encrypt

--- src/main/java/lab3/lib2/AES.java
+++ src/main/java/lab3/lib2/AES.java
@@ -3,12 +3,24 @@ package lab3.lib2;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import javax.crypto.Cipher;
+import javax.crypto.CipherOutputStream;
+
 public class AES {
 	
 	public static final String ALGORITHM = "AES/CBC/PKCS5Padding";
 
-	void encrypt(InputStream src, OutputStream out) {
-		
+	public int encrypt(InputStream src, OutputStream out) {
+		try {
+			Cipher cipher = Cipher.getInstance(ALGORITHM);
+			try(CipherOutputStream cout = new CipherOutputStream(out, cipher)) {
+				StreamCopier copier = new StreamCopier(src, cout);
+				return copier.copyAll();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("No AES support", e);
+		}
 	}
 
 	void decrypt(InputStream src, OutputStream out) {

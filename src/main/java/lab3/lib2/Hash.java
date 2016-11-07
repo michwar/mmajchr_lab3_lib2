b12b59e78017fa307a8693b2fba6b7dfb97a213a
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-07 23:06:22 +0100

    pole md w klasie Hash

--- src/main/java/lab3/lib2/Hash.java
+++ src/main/java/lab3/lib2/Hash.java
@@ -1,9 +1,14 @@
 package lab3.lib2;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Hash {
 	
-	public Hash(String algo) {
-		
+	MessageDigest md;
+	
+	public Hash(String algorithm) throws NoSuchAlgorithmException {
+		this.md = MessageDigest.getInstance(algorithm);
 	}
 
 }

6c26e3a4b0bf957286803dfbc8907ccace727762
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-07 23:09:10 +0100

    konstruktor Hash przyjmujacy HashType

--- src/main/java/lab3/lib2/Hash.java
+++ src/main/java/lab3/lib2/Hash.java
@@ -10,5 +10,14 @@ public class Hash {
 	public Hash(String algorithm) throws NoSuchAlgorithmException {
 		this.md = MessageDigest.getInstance(algorithm);
 	}
+	
+	public Hash(HashType type) {
+		try {
+			this.md = MessageDigest.getInstance(type.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("No " + type + " support.", e);
+		}
+	}
 
 }

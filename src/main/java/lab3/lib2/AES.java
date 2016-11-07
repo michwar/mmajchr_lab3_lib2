793dcfbd1ddb2a6deee7994fcc714ec907464abc
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-07 22:26:56 +0100

    rzucanie dalej IOException

--- src/main/java/lab3/lib2/AES.java
+++ src/main/java/lab3/lib2/AES.java
@@ -1,5 +1,6 @@
 package lab3.lib2;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
@@ -11,26 +12,30 @@ public class AES {
 	
 	public static final String ALGORITHM = "AES/CBC/PKCS5Padding";
 
-	public int encrypt(InputStream in, OutputStream out) {
+	public int encrypt(InputStream in, OutputStream out) throws IOException {
 		try {
 			Cipher cipher = Cipher.getInstance(ALGORITHM);
 			try(CipherOutputStream cout = new CipherOutputStream(out, cipher)) {
 				StreamCopier copier = new StreamCopier(in, cout);
 				return copier.copyAll();
 			}
+		} catch (IOException e) {
+			throw e;
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new RuntimeException("No AES support", e);
 		}
 	}
 
-	public int decrypt(InputStream in, OutputStream out) {
+	public int decrypt(InputStream in, OutputStream out) throws IOException {
 		try {
 			Cipher cipher = Cipher.getInstance(ALGORITHM);
 			try(CipherInputStream cin = new CipherInputStream(in, cipher)) {
 				StreamCopier copier = new StreamCopier(cin, out);
 				return copier.copyAll();
 			}
+		} catch (IOException e) {
+			throw e;
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new RuntimeException("No AES support", e);

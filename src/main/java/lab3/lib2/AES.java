68fb66fbc4f748699f0bb11305b0be4789120a2a
Michal Warcholinski <187834@edu.p.lodz.pl>
2016-11-07 22:12:58 +0100

    metody encrypt i decrypt w klasie AES

--- src/main/java/lab3/lib2/AES.java
+++ src/main/java/lab3/lib2/AES.java
@@ -1,5 +1,16 @@
 package lab3.lib2;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class AES {
+
+	void encrypt(InputStream src, OutputStream out) {
+		
+	}
+
+	void decrypt(InputStream src, OutputStream out) {
+		
+	}
 
 }
